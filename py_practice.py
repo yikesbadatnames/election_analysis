a883e4f0e3932d286776a29399cb7b84750b59c8
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-08-30 15:03:47 -0700

    finished lists section

--- py_practice.py
+++ py_practice.py
@@ -1,4 +1,37 @@
 print("Hello Word")
 counties = ["Arapahoe","Denver","Jefferson"]
 
-print(counties[:2])
+# Add El Paso to the second position in the list. 
+
+counties.insert(1, "El Paso")
+
+# Remove Arapahoe from the list. 
+
+counties.remove("Arapahoe")
+
+# Make Denver the third item in the list, but keep Jefferson the second item in the list.  
+
+counties.remove("Denver") 
+
+counties.insert(2,"Denver")
+
+# Add Arapahoe to the list. 
+
+counties.append("Arapahoe")
+
+print(counties)
+
+counties.append("El Paso")
+
+counties.insert(2,"El Paso")
+
+counties.remove("El Paso")
+
+counties.pop(3)
+
+counties[2] = "El Paso"
+
+print(counties)
+
+# Lists and list comands
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
