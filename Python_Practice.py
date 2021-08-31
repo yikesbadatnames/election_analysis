f5990e91cc4ce2bb928d7b8b7d6b88d704d09cd1
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-08-31 12:55:14 -0700

    Membership Operators

--- Python_Practice.py
+++ Python_Practice.py
@@ -8,3 +8,27 @@ if temperature > 80:
     print("turn on the ac")
 else:
     print("open the windows")
+
+
+print("Starting Elif Section -----------------------------------------------------")
+# What is the score
+score = int(input("What is your test score? "))
+
+# Determine the grade
+
+if score >= 90:
+    print('Your grade is an A')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F... Donkey')
+
+
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is in Texas... Right?")
