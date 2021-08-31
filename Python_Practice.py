8a332c276b7aedf14a54fd4b05c1178cb98387c1
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-08-30 18:13:01 -0700

    if and if-else

---
+++ Python_Practice.py
@@ -0,0 +1,10 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == "Denver":
+    print(counties[1])
+
+temperature = int(input("what is the tempature outside? "))
+
+if temperature > 80:
+    print("turn on the ac")
+else:
+    print("open the windows")
