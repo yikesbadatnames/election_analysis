895d45284e797ce134cd828150d6c2bd3148faa7
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-08-30 17:30:59 -0700

    list of dictionaries

--- py_practice.py
+++ py_practice.py
@@ -54,6 +54,29 @@ print(counties_dict.values())
 print(counties_dict.get("Arapahoe"))
 print(counties_dict['Arapahoe'])
 
-
 #Dictionaries
 #^^^^^^^^^^^^^^^^^^^^^^^^^
+
+voting_data = []
+voting_data.append({"county":"Arapahoe", "registered_voters":422829})
+voting_data.append({"county":"Denver","registered_voters":463353})
+voting_data.append({"county":"Jefferson","registered_voters":432438})
+
+#Add the new county “El Paso” and its registered voters, 461149, to the second position in voting_data. 
+voting_data.insert(1,{"county":"El Paso", "registered voters":461149})
+
+#Remove “Arapahoe” and its registered voters from voting_data. 
+
+voting_data.remove({"county":"Arapahoe", "registered_voters":422829})
+
+
+#Make “Denver” and its registered voters the third item in voting_data, but keep “Jefferson” and its registered voters as the second item.   
+voting_data.remove({"county":"Denver","registered_voters":463353})
+voting_data.insert(2,{"county":"Denver","registered_voters":463353})
+#Add “Arapahoe” and its registered voters to voting_data.
+voting_data.append({"county":"Arapahoe","registered_voters":461149})
+
+print(voting_data)
+
+# List of dictionaries
+# ^^^^^^^^^^^^^^^^^^^^^^^^^
