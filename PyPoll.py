acc461edfcdccb4f8b477240afe830370df2edc9
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-09-01 19:13:31 -0700

    printing all csv

--- PyPoll.py
+++ PyPoll.py
@@ -1,17 +1,13 @@
 import csv
 import os
-# Assign a variable for the file to load and the path.
-file_to_load = os.path.join("resources", "election_results.csv")
-# assign a variable for the file to load and the path.
-with open(file_to_load) as election_data:
-#open the election results and read the file.
-# to do: perform analysis
-    print(election_data)
-#close file
-election_data.close()
-#creat a file name variable to a direct or indirect path to the file
+# assign a variable to load a file from a path
+file_to_load = os.path.join("resources","election_results.csv")
+#assign a variable to save the file to a path
 file_to_save = os.path.join("analysis","election_analysis.txt")
-#using the with statement open the file as a text file
-with open(file_to_save,"w") as txt_file:
-#write some data to the file
-    txt_file.write("Araphaoe\nDenver\nJefferson")
+
+# open the election results and read the file
+with open(file_to_load) as election_data:
+    file_reader =  csv.reader(election_data)
+    #read and print each header
+    for row in file_reader:
+        print(row)
