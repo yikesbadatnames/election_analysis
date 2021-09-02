157e5eb69f91c6e0baf5b311783bc7572858db1c
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-09-01 18:38:18 -0700

    opening, creating, and writing in a new file

--- PyPoll.py
+++ PyPoll.py
@@ -1,25 +1,23 @@
-# The data we need to retrieve
-# open the election results and read the file
-file_to_load = os.path.join("resources/election_results.csv")
-# 1. The total number of votes cast
-election_data = open(file_to_load,"r")
-# 2. a complete list of candidates who recieved votes
-# 3. the percentage of votes each candidate won
-# 4. the total number of votes each candidate won
-# 5. the winner of the election based on popular vote
-
-
-
-
-
-#Practice work for class
-print(election_data)
-#close the file
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("resources", "election_results.csv")
+# assign a variable for the file to load and the path.
+with open(file_to_load) as election_data:
+#open the election results and read the file.
+# to do: perform analysis
+    print(election_data)
+#close file
 election_data.close()
-#Example of Dependencies (datetime), Modeule (datetime), Package (now)
-# Import the datetime class from the datetime module.
-#import datetime
-# Use the now() attribute on the datetime class to get the present time.
-#now = datetime.datetime.now()
-# Print the present time.
-#print("The time right now is ", now)
+#creat a file name variable to a direct or indirect path to the file
+file_to_save = os.path.join("analysis","election_analysis.txt")
+#using the open() function with the "w" mode we will write the data
+#to the file
+outfile = open(file_to_save,"w")
+#write some data to the file
+outfile.write("Mom's yer Dad 2")
+#close the file
+
+
+   
+# close the file
